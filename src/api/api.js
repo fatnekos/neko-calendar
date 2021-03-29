9e3d10bde8280f6ced2e6584d7a8c23581d3d6a2
Hao Chun Chang <changhaochun84@gmail.com>
2021-03-29 23:50:36 +0800

    Add api handler function

---
+++ src/api/api.js
@@ -0,0 +1,6 @@
+// API Handlers
+
+export const onTextInput = async (input_text) => {
+  // Triggered when user finished entering text.
+  // Send POST request to backend server to save text into db.
+}
